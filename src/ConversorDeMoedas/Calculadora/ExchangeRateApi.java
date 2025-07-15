ebb93e232d84afd517d0ada11e551b78757b239c
Pedr05TW3 <pedroeusoudoscosta@gmail.com>
2025-07-15 19:58:11 -0300

    Projeto de Conversão de moedas

---
+++ src/ConversorDeMoedas/Calculadora/ExchangeRateApi.java
@@ -0,0 +1,12 @@
+package ConversorDeMoedas.Calculadora;
+
+public record ExchangeRateApi(String base_code, String target_code, String time_last_update_utc, String conversion_rate){
+    @Override
+    public String toString() {
+        return "\nConversão: " +
+                "\nMoeda a ser convertida: " + base_code +
+                "\nConverter para: " + target_code +
+                "\nÚltima atualização: " + time_last_update_utc +
+                "\nResultado: " + conversion_rate;
+    }
+}
